db15c2d7e766f3ecac3d73b57e18a03b424b8762
allansomensi <allansomensidev@proton.me>
2026-02-20 10:02:49 -0300

    build(windows): :wrench: add manifest to require admin rights

---
+++ crates/rindrive/build.rs
@@ -0,0 +1,20 @@
+fn main() {
+    set_windows_exe_options();
+}
+
+fn set_windows_exe_options() {
+    static MANIFEST: &str = "pkg/windows/manifest.xml";
+
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+    let profile = std::env::var("PROFILE").unwrap_or_default();
+
+    if target_os == "windows" && profile == "release" {
+        let mut res = winres::WindowsResource::new();
+        res.set_manifest_file(MANIFEST);
+
+        if let Err(e) = res.compile() {
+            eprintln!("Error: Failed to compile Windows resources: {e}");
+            std::process::exit(1);
+        }
+    }
+}
